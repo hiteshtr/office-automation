608a53e14af136d0080507e2597a581e66fad6cb
hitesh trivedi <hiteshtr@gmail.com>
2013-01-21 12:57:01 +0530

    change in migration file

--- db/migrate/20130118064110_create_leave_details.rb
+++ db/migrate/20130118064110_create_leave_details.rb
@@ -1,18 +1,18 @@
 class CreateLeaveDetails < ActiveRecord::Migration
   def change
     create_table :leave_details, {:id=>false} do |t|
-      t.string  :leave_id
-      t.string  :reg_no
-      t.date    :leave_from
-      t.date    :leave_to
-      t.integer :leave_duration
-      t.text    :leave_reason
-      t.boolean :academic_status
-      t.boolean :warden_status
-      t.boolean :other_status
+      t.integer  :leave_id
+      t.string  :reg_no, :null => false
+      t.date    :leave_from, :null => false
+      t.date    :leave_to, :null => false
+      t.integer :leave_duration, :null => false
+      t.text    :leave_reason, :null => false
+      t.boolean :academic_status, :default => '0', :null => false
+      t.boolean :warden_status, :default => '0', :null => false
+      t.boolean :other_status, :default => '0', :null => false
 
       t.timestamps
-      end
-	  execute "ALTER TABLE leave_details ADD PRIMARY KEY (leave_id);"
+    end
+      execute "ALTER TABLE leave_details ADD PRIMARY KEY (leave_id);"
   end
 end
